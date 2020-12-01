14121b7564f4e203f7653a0b9543e1216d3a9993
devlog-t <devlog.code@gmail.com>
2020-12-01 07:26:17 +0100

    push

--- Moving-Zeros-To-The-End.js
+++ Moving-Zeros-To-The-End.js
@@ -1,8 +1,7 @@
 var moveZeros = function (arr) {
-    var moveZeros = function (arr) {
-        return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
-      }
+    return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
 }
+
 
 console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
 console.log(moveZeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]))

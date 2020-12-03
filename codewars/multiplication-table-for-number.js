369c70466ad4c6937bd01d14805054333eb3a7e8
devlog-t <devlog.code@gmail.com>
2020-12-03 17:23:38 +0100

    Multiplication-table-for-number

--- codewars/multiplication-table-for-number.js
+++ codewars/multiplication-table-for-number.js
@@ -1,10 +1,25 @@
 function multiTable(number) {
     let table = "";
-    for(let i = 1; i < 10; i++){
-      table += i + " * " + number + " = " + (i * number) + "\n"
+    for(let i = 1; i < 11; i++){
+      if(i === 10){
+        table += i + " * " + number + " = " + (i * number)
+  } else {
+    table += i + " * " + number + " = " + (i * number) + "\n"
+  }
+      
     }
     return table
   }
 
   // \n en trop sur dernier élément
-  console.log(multiTable(2))
+  console.log(multiTable(4))
+
+  const multiTable = (number) => {
+    let table = '';
+    
+    for(let i = 1; i <= 10; i++) {
+      table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+    }
+  
+    return table;
+  }

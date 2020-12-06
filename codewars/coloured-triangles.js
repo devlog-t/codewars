c579ea659fcc46eb8c181c6130d75b87aab584b3
devlog-t <devlog.code@gmail.com>
2020-12-06 17:05:41 +0100

    push

--- codewars/coloured-triangles.js
+++ codewars/coloured-triangles.js
@@ -1,16 +1,27 @@
-function triangle(row) {
-    let cut = row.split('') //récupérer chaque lettre
-    let nextStep = []
-    for(let i = 0; i < cut.length; i++){
-        // console.log(cut[i])
-        if(cut[i] === cut[i+1]){
-            nextStep.push(cut[i])
+let rules = {R: ['BG', 'GB', 'RR'], B: ['RG', 'GR', 'BB'], G: ['BR', 'RB', 'GG']}
 
+// console.log(rules.R.includes('GB'))
+
+function triangle(row) {
+  let arr = row.split('')
+
+  while(arr.length != 1){
+  arr = arr.map((element, index, basic_arr) => 
+    {
+      if(basic_arr[index + 1] != undefined){
+        for (const key in rules) {
+           if(rules[key].includes(element + basic_arr[index + 1])) return key
         }
+      }
     }
-    console.log(nextStep)
-    
+  )
+  arr.splice(-1)
+  }
+  
+  return arr.join()
 }
+
+triangle('B') //B
 
 triangle('GB')
 // triangle('RRR')

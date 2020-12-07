5ff3bea50275b233e7376ef7dedd1a7f3db8388e
devlog-t <devlog.code@gmail.com>
2020-12-07 17:57:59 +0100

    push

--- soutenance-kercode/decipher-this.js
+++ soutenance-kercode/decipher-this.js
@@ -1,47 +1,35 @@
 function decipherThis(str) {
-    // let letter = {
-    //     97: 'a',
-    //     98: 'b',
-    //     99: 'c',
-    //     100: 'd',
-    //     101: 'e',
-    //     102: 'f',
-    //     103: 'g',
-    //     104: 'h',
-    //     105: 'i',
-    //     106: 'j',
-    //     107: 'k',
-    //     108: 'l',
-    //     109: 'm',
-    //     110: 'n',
-    //     111: 'o',
-    //     112: 'p',
-    //     113: 'q',
-    //     114: 'r',
-    //     115: 's',
-    //     116: 't',
-    //     117: 'u',
-    //     118: 'v',
-    //     119: 'w',
-    //     120: 'x',
-    //     121: 'y',
-    //     122: 'z',
-    // }
-
-
+    
     let arr = str.split(' ')
+    let result = []
 
     for(let i = 0; i < arr.length; i++){
-        // console.log(arr[i])
-        let match = arr[i].match(/(\d+)/)
-        console.log(arr[i].charCodeAt(match[0]))
-    }
-    
-    //Récupérer valeur de match et la replace avec chart code
-    //Intervertir index 1 et dernier index avec variable transitoire
-    
 
-    return arr
+        //Récupérer les lettres uniquement
+        let matchLetter = arr[i].match(/[a-z]/g)
+        
+        // Chiffre seul = null
+        if(matchLetter){
+            let a = matchLetter[0]
+            let b = matchLetter[matchLetter.length - 1]
+            matchLetter[0] = b
+            matchLetter[matchLetter.length - 1] = a
+        } else {
+            matchLetter = [""]
+        }
+
+        // Récupérer les chiffres et les convertir en lettre
+        let matchNum = arr[i].match(/(\d+)/)
+        let translate = String.fromCharCode(parseInt(matchNum[0], 0))
+
+        matchLetter.unshift(translate)
+        // console.log(translate)
+        result.push(matchLetter.join(''))
+        
+        //join with space    
+    }
+
+    return result.join(' ')
     
 }; 
 
